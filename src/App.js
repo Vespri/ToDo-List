c5d47c4f35954e5005ed4f093a1687dd1f1ff546
Kresna Vespri Wijaya <83352781+Vespri@users.noreply.github.com>
2022-06-22 03:55:55 +0700

    add delete todoList feature

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
-import React, {useState} from 'react';
-import Header from './components/Header';
-import Form from './components/Form';
-import TodosList from './components/TodosList';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import TodosList from "./components/TodosList";
+import "./App.css";
 
 const App = () => {
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState([]);
 
-  return (<div className="container">
-    <div className="app-wrapper">
-      <div>
-        <Header />
-      </div>
-      <div>
-        <Form 
-        input = {input}
-        setInput = {setInput}
-        todos = {todos}
-        setTodos = {setTodos}
-        />
-      </div>
-      <div>
-        <TodosList todos={todos} setTodos={setTodos} />
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        </div>
+        <div>
+          <TodosList todos={todos} setTodos={setTodos} />
+        </div>
       </div>
     </div>
-  </div>
   );
-}
+};
 
 export default App;
