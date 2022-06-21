ddfcc5117cedad94dbb9f847802198d04f13428f
Kresna Vespri Wijaya <83352781+Vespri@users.noreply.github.com>
2022-06-22 02:49:33 +0700

    add component files (Form.js, Header.js, TodoList.js) and modify some files. 40% to finishing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Header from './components/Header';
+import Form from './components/Form';
+import TodosList from './components/TodosList';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      App
+const App = () => {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  return (<div className="container">
+    <div className="app-wrapper">
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Form 
+        input = {input}
+        setInput = {setInput}
+        todos = {todos}
+        setTodos = {setTodos}
+        />
+      </div>
+      <div>
+        <TodosList todos={todos} setTodos={setTodos} />
+      </div>
     </div>
+  </div>
   );
 }
 
